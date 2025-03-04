3ad729f02649a2c18ad148b39252e8491731e4de
Oxayr <uzairmaqsood451@gmail.com>
2025-03-04 11:58:20 +0500

    added controller for create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function create(Requests $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|decimal:0,2',   // 0 is the minimum and 2 is the maximum
+            'description' => 'nullable',
+            'qty' => 'required|numeric'  // it is neccessary to write the type of the table 
+        ]);
+        $newProduct = Product::create($data);
+    }
 }
